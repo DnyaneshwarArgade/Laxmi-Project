eee4d857d5b5ce0da690eee9946a5623851e43d8
Dnyaneshwar Argade <dnyaneshwarargade8@gmail.com>
2025-08-13 18:50:16 +0530

    Bottom Bar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { Routes, Route, NavLink } from "react-router-dom";
+import Items from "./Components/Items";
+import Order from "./Components/Order";
+import Search from "./Components/Search";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="app-container">
+      {/* Main Content */}
+      <div className="page-content">
+        <Routes>
+          <Route path="/" element={<Order />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </div>
 
-export default App
+      {/* Bottom Tab Bar */}
+      <nav className="bottom-tab">
+        <NavLink to="/items" className="tab-item">
+          🛒 Items
+        </NavLink>
+        <NavLink to="/" className="tab-item">
+          📦 Order
+        </NavLink>
+        <NavLink to="/search" className="tab-item">
+          🔍 Search
+        </NavLink>
+      </nav>
+    </div>
+  );
+}
--- src/Componenet/Items.jsx
+++ src/Componenet/Items.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Items() {
   return (
     <div>
-      
+      <h2>items pageeeeeeeeeeeeeeeeeeeeeeeeeee</h2>
     </div>
   )
 }

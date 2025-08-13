ad40980547b4a85acd17217fd0cc432a130a198a
sakshi-pawar106 <pawarsakshi141p@gmail.com>
2025-08-13 22:55:43 +0530

    Search Page Created

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,3 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
-// main.jsx (Vite) or index.js (CRA)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";

eb885c413bb87448c8e885f3d423b309d61e5092
sakshi-pawar106 <pawarsakshi141p@gmail.com>
2025-08-18 18:59:45 +0530

    Customer Page Almost Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+// import { Routes, Route, NavLink } from "react-router-dom";
+// import Items from "./Component/Items";
+// import Order from "./Component/Order";
+// import Customer from "./Component/Customer";
+// import "./App.css";
+
+// export default function App() {
+//   return (
+//     <div className="app-container bg-light">
+//       {/* Main Content */}
+//       <div className="page-content">
+//         <Routes>
+//           <Route path="/" element={<Order />} />
+//           <Route path="/items" element={<Items />} />
+//           <Route path="/customer" element={<Customer/>} />
+          
+//         </Routes>
+//       </div>
+
+//       {/* Bottom Tab Bar */}
+//       <nav className="bottom-tab">
+//         <NavLink to="/items" className="tab-item">
+//           <span className="icon">🛒</span>
+//           <span className="label">Items</span>
+//         </NavLink>
+//         <NavLink to="/" className="tab-item">
+//           <span className="icon">📦</span>
+//           <span className="label">Order</span>
+//         </NavLink>
+//         <NavLink to="/search" className="tab-item">
+//           <span className="icon"></span>
+//           <span className="label">Customer</span>
+//         </NavLink>
+//       </nav>
+//     </div>
+//   );
+// }
+
 import { Routes, Route, NavLink } from "react-router-dom";
 import Items from "./Component/Items";
 import Order from "./Component/Order";
-import Search from "./Component/Search";
+import Customer from "./Component/Customer";
 import "./App.css";
 
 export default function App() {
@@ -12,7 +50,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Order />} />
           <Route path="/items" element={<Items />} />
-          <Route path="/search" element={<Search />} />
+          <Route path="/customer" element={<Customer />} />
         </Routes>
       </div>
 
@@ -26,11 +64,12 @@ export default function App() {
           <span className="icon">📦</span>
           <span className="label">Order</span>
         </NavLink>
-        <NavLink to="/search" className="tab-item">
-          <span className="icon">🔍</span>
-          <span className="label">Search</span>
+        <NavLink to="/customer" className="tab-item">
+          <span className="icon">👥</span>   {/* Customer साठी icon */}
+          <span className="label">Customer</span>
         </NavLink>
       </nav>
     </div>
   );
 }
+

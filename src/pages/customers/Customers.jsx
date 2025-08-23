2f037d01182b81e429e51b7c3eb90428a0cd1461
Dnyaneshwar Argade <dnyaneshwarargade8@gmail.com>
2025-08-23 11:53:09 +0530

    API Added

--- src/pages/customers/Customers.jsx
+++ src/pages/customers/Customers.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import * as actions from "../../store/creators";
 
 const Customers = () => {
+  const dispatch = useDispatch()
+  const { login } = useSelector((state) => state.login)
+  const { customers } = useSelector((state) => state.entities.customers)
+
+  console.log('customers', customers)
+  const data = {
+    token: login.token
+  }
+  useEffect(() => {
+    dispatch(actions.customersGetData(data));
+  }, []);
   return (
-    <div>Customers</div>
+    <div>
+      {customers.data && customers.data.map((item) => {
+        return (
+          <div>{item.name}</div>
+        )
+      })}
+    </div>
   )
 }
 
--- src/pages/items/Items.jsx
+++ src/pages/items/Items.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import * as actions from "../../store/creators";
 
 const Items = () => {
+  const dispatch = useDispatch()
+  const { login } = useSelector((state) => state.login)
+  const { items } = useSelector((state) => state.entities.items)
+
+  console.log('items', items)
+  const data = {
+    token: login.token
+  }
+  useEffect(() => {
+    dispatch(actions.itemsGetData(data));
+  }, []);
   return (
-    <div>items</div>
+    <div>
+      {items.data && items.data.map((item) => {
+        return (
+          <div>{item.name}</div>
+        )
+      })}
+    </div>
   )
 }
 
